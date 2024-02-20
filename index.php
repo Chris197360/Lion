ed1da98dda57e7311aab8c1b0024394598606a5e
Chris <christophe.podlunsek@yahoo.fr>
2024-02-20 09:50:32 +0100

    Version 1

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="L'Agence LUIS IMMOBILIER, situé près de la zone commerciale à Fitz-James
+     dans l’Oise (60), est spécialisé dans le domaine de la vente et de la location de biens immobilier.">
+    <meta name="keywords" content="">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/styletelephone.css">
+    <title>Accueil</title>
+</head>
+
+<body>
+    <?php
+    include "./includes/header.php";
+    ?>
+    <main>
+        <h1>Quelques annonces de notre agence</h1>
+
+        <p>Quels que soient votre état d’esprit et le projet immobilier qui vous amènent, nos professionnels de
+            l’immobilier,
+            véritables acteurs engagés depuis 2020 ont la chance d’exercer l’un des plus beaux métiers du monde ».
+            Ce métier consiste à vous accompagner dans :<br>
+            - vente d’un appartement, la vente d'une maison,<br>
+            - achat d’un appartement,<br>
+            - achat d’une maison,<br>
+            - location d’un appartement, d’une maison ou sa mise en location,<br>
+            - ainsi que sa gestion locative.<br>
+            <br>
+            Dans l’ancien ou dans le neuf, appartements, maisons principales ou secondaires... Que vous soyez en solo,
+            en duo ou plus nombreux, nous souhaitons vous offrir une vie la plus épanouie possible. Nous espérons faire
+            votre connaissance prochainement dans notre agence immobilière.<br>
+            <br>
+            Et en attendant, et comme le marché de l'immobilier est en constante évolution, nous vous invitons à
+            consulter
+            les prix de l'immobilier partout dans l'Oise. Grâce à notre expérience sur le terrain, nous avons collecté
+            une multitude
+            de datas qui seront une véritable aide à la décision en vue de votre prochaine vente ou de votre prochain
+            achat.
+        </p><br>
+    </main>
+
+
+</body>
+
+</html>
